ddfc8cb51412ce6a3c3e96103f27a4c9938329d1
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-04-04 21:09:38 +0530

    Added a simple C++ program to calculate the sum of two numbers using a function

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int add(int,int);
+
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"The sum is "<<add(a,b);
+}
+
+int add(int x,int y)
+{
+    return (x+y);
+}
